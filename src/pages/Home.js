cdd44045bf5cf1aa2692b6028fcc352c26a2668c
Lovish-Aggarwal <lovishagg@gmail.com>
2021-08-10 16:17:58 +0530

    added radio buttons for search options.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-import {apiGet} from '../misc/config'
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
-  const [results,setResults]=useState(null);
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsearch = searchOption === 'shows';
 
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=men
 
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
       // eslint-disable-next-line
       console.log(result);
@@ -21,29 +24,69 @@ const Home = () => {
     setInput(ev.target.value);
   };
 
-  const onKeyDown= ev=>{
-    if(ev.keyCode===13)
-    onSearch();
-  }
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) onSearch();
+  };
 
-  const renderResults=()=>{
-    if(results && results.length===0)
-    {
-      return <div>No Results</div>
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  // eslint-disable-next-line
+  // console.log(searchOption);
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
     }
 
-    if(results && results.length>0)
-    {
-      return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+    if (results && results.length > 0) {
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
 
     return null;
-  }
+  };
 
   return (
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
-      <button type="button" onClick={onSearch}>Search</button>
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
+      <div>
+        <label htmlFor="shows-search">
+          Show
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
       {renderResults()}
     </MainPageLayout>
   );
