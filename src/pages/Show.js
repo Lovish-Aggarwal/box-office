950ca0cf9783f275d6f21e3d9c5d16a0939bad0d
Lovish-Aggarwal <lovishagg@gmail.com>
2021-08-12 19:08:24 +0530

    Added Loading and Error messages in show page using useEffect hooks.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,29 +1,50 @@
 /* eslint-disable no-unused-vars */
-import React,{ useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 const Show = () => {
-    const { id } = useParams();
-    const [show,setShow]=useState(null);
-    // eslint-disable-next-line
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  // eslint-disable-next-line
 
-    useEffect(()=>{
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results =>{
-            setShow(results);
-        });
+  useEffect(() => {
+    let isMounted = true;
 
-    },[id])
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
 
-    // eslint-disable-next-line
-    console.log('show',show);
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
 
-    return (
-        <div>
-            This is Show Page.
-        </div>
-    )
-}
+  // eslint-disable-next-line
+  console.log('show', show);
 
-export default Show
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>error occured:{error}</div>;
+  }
+
+  return <div>This is Show Page.</div>;
+};
+
+export default Show;
